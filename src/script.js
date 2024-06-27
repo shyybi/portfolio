79808513d702e9434e4219179a26e613bba65338
Feyli <mathiscom64@gmail.com>
2024-06-28 00:22:40 +0200

    hiding scrolling indicator when scrolling
    Took 14 minutes

--- src/script.js
+++ src/script.js
@@ -1,12 +1,3 @@
-replacements = ["making the website look good", "adding fonts", "playing with animations", "struggling with styles", "trying to exit vim", "doing commits to break everything", "giving up and letting down", "watching an indian tutorial"]
-
-const text = document.querySelector("header p").innerText.replace("[replace]", replacements[Math.floor(Math.random() * replacements.length)]);
-const speed = 50;
-
-document.querySelector("header p").innerText = "";
-
-for (let i = 0; i < text.length; i++) {
-    setTimeout(function () {
-        document.querySelector("header p").innerHTML += text[i] === "&nbsp;" ? "" : text[i];
-    }, speed * i);
-}
+document.querySelector(".slides").onscroll = () => {
+  document.querySelector(".scrollsvg").style.opacity = 0;
+};
