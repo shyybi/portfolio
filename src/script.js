5261bf972ddd6f87ab90095365b9f7d8fd646c21
Feyli <mathiscom64@gmail.com>
2024-07-02 00:17:27 +0200

    - added gradient background - added text - added wave emoji - added wave animation to wave emoji - tweaked the scrolling indicator to not show under certain screen heights - fixed a bug where the scrolling indicator would stay clickable after disappearing
    Took 1 hour 38 minutes

--- src/script.js
+++ src/script.js
@@ -1,5 +1,10 @@
 slideDiv = document.querySelector(".slides");
+scrollSvg = document.querySelector(".scrollsvg");
 
 slideDiv.addEventListener("scroll", function() {
   document.querySelector(".scrollsvg").style.display = "none";
+}, { once: true });
+
+scrollSvg.addEventListener("click", function () {
+  document.querySelector(".slide2").scrollIntoView({ behavior: "smooth" });
 }, { once: true });
