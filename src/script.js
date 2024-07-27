2fddb6ec573ecab9fca267dff64cbde0ce48e847
Feyli <mathiscom64@gmail.com>
2024-07-27 18:41:56 +0200

    - changed the second page's intro text - added french colors to the first page on hover - set up the hint animation for every slideshow - changed some texts - changed some font variations

--- src/script.js
+++ src/script.js
@@ -8,3 +8,33 @@ slideDiv.addEventListener("scroll", function() {
 scrollSvg.addEventListener("click", function() {
   document.querySelector(".slide2").scrollIntoView({ behavior: "smooth" });
 }, { once: true });
+
+// Function to check if the element is visible in the viewport
+const elementIsVisibleInViewport = (el, partiallyVisible = false) => {
+  const { top, left, bottom, right } = el.getBoundingClientRect();
+  const { innerHeight, innerWidth } = window;
+  return partiallyVisible
+    ? ((top > 0 && top < innerHeight) ||
+        (bottom > 0 && bottom < innerHeight)) &&
+        ((left > 0 && left < innerWidth) || (right > 0 && right < innerWidth))
+    : top >= 0 && left >= 0 && bottom <= innerHeight && right <= innerWidth;
+};
+
+// Function to loop until the element is visible and then execute the instruction
+const waitForElementToBeVisible = (selector, instruction, partiallyVisible = false, checkInterval = 100) => {
+  const interval = setInterval(() => {
+    const element = document.querySelector(selector);
+    if (element && elementIsVisibleInViewport(element, partiallyVisible)) {
+      clearInterval(interval); // Stop the interval once the element is visible
+      instruction(); // Execute the instruction
+    }
+  }, checkInterval); // Check every 100ms by default
+};
+
+// Run for the slideshow
+waitForElementToBeVisible('.slideshow', () => {
+  setTimeout(() => {
+    document.querySelectorAll('.project').forEach((object) => object.classList.add('hintmove'));
+  }, 1000);
+});
+
