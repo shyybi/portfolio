486c1a9d2a8f3157c536047d51a0335f9bae0e38
Feyli <mathiscom64@gmail.com>
2024-08-01 03:49:08 +0200

    javascript: - fixed conflicts in variables - added isOverflown function to scroll_handler.js - fixed an incorrect name in a listener - added arrow-related listeners
    html/css:
    - added arrows in the project page to help mouse users navigate the projects
    - added animation for the arrows
    - tweaked the hover effect on the social links of the second page
    - removed the hintMove animation for mouse users

--- src/scroll_handler.js
+++ src/scroll_handler.js
@@ -1,13 +1,16 @@
 const slideDiv = document.querySelector(".slides");
 const scrollSvg = document.querySelector(".scrollsvg");
-const interestSlideshow = document.querySelector(".passions .slideshow");
+
+function isOverflown(element) {
+  return element.scrollHeight > element.clientHeight || element.scrollWidth > element.clientWidth;
+}
 
 slideDiv.addEventListener("scroll", function() {
   document.querySelector(".scrollsvg").style.display = "none";
 }, { once: true });
 
 scrollSvg.addEventListener("click", function() {
-  document.querySelector(".slide2").scrollIntoView({ behavior: "smooth" });
+  document.querySelector("#about").scrollIntoView({ behavior: "smooth" });
 }, { once: true });
 
 const pointerScroll = (elem) => {
@@ -27,3 +30,12 @@ setInterval(() => {
   if (!isOverflown(interestSlideshow)) interestSlideshow.classList.remove("horizontal-scroll");
   else interestSlideshow.classList.add("horizontal-scroll");
 }, 100);
+
+// Add event listeners to both arrows next to the projects slideshow to scroll the slideshow
+document.querySelector(".left-arrow").addEventListener("click", () => {
+  document.querySelector(".project-page .slideshow").scrollBy({ left: -document.querySelector('.project').clientWidth - 40, behavior: "smooth" });
+});
+
+document.querySelector(".right-arrow").addEventListener("click", () => {
+  document.querySelector(".project-page .slideshow").scrollBy({ left: document.querySelector('.project').clientWidth + 40, behavior: "smooth" });
+});
